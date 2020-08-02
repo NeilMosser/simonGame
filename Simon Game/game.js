9175be08efd18de8f357a3cca6854ee3a28159d6
NeilMosser <66801361+NeilMosser@users.noreply.github.com>
2020-08-02 11:59:04 -0600

    Initial commit of files

---
+++ Simon Game/game.js
@@ -0,0 +1,89 @@
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+// Create arrays for the game pattern and the user selections
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+// Start the game
+$(document).keypress(function() {
+  if(!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+
+});
+
+// On click, push user color to userClickedPattern array, animate and play sound
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+// Checks the user answer against the pattern
+function checkAnswer(currentLevel) {
+if(userClickedPattern[currentLevel]  === gamePattern[currentLevel]) {
+  if (userClickedPattern.length === gamePattern.length) {
+    setTimeout(function() {
+      nextSequence();
+    }, 1000);
+    }
+  }
+
+  // if not true, play wrong sound, flash red, and allow user to start over
+ else {
+  playSound("wrong");
+  $("body").addClass("game-over");
+  setTimeout(function(){
+    $("body").removeClass("game-over");
+  }, 200);
+  $("#level-title").text("Game Over, Press Any Key to Restart");
+  startOver();
+}
+
+}
+
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+
+}
+
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+
+
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
